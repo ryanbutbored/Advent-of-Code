b0381736f62564adf44ae96ac6f09983f17897ae
ryanbutbored <94403867+ryanbutbored@users.noreply.github.com>
2023-12-05 11:18:14 +0000

    day 2 part 2

---
+++ Day 2/Part 2.py
@@ -0,0 +1,24 @@
+f = open("Day 2\\input.txt", "r")
+lines = f.readlines()
+total = 0
+for line in lines:
+    maxRed = 0
+    maxGreen = 0
+    maxBlue = 0
+    line = line.strip().split(":")
+    sets = line[1].split(";")
+    for set in sets:
+        subset = set.split(",")
+        for cubes in subset:
+            if "red" in cubes:
+                if int(cubes.replace("red", "").replace(" ", "")) > maxRed:
+                    maxRed = int(cubes.replace("red", "").replace(" ", "")) 
+            elif "green" in cubes:
+                if int(cubes.replace("green", "").replace(" ", "")) > maxGreen:
+                    maxGreen = int(cubes.replace("green", "").replace(" ", ""))
+            else:
+                if int(cubes.replace("blue", "").replace(" ", "")) > maxBlue:
+                    maxBlue = int(cubes.replace("blue", "").replace(" ", ""))
+    power = maxRed * maxGreen * maxBlue
+    total += power
+print(total)
