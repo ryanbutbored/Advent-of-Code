077f344a6133d74fd51a14d0ffc560bcfcadc0d3
ryanbutbored <94403867+ryanbutbored@users.noreply.github.com>
2023-12-05 10:12:48 +0000

    day 1 part 2

--- Day 1.py
+++ Day 1.py
@@ -1,16 +1,31 @@
+def searchLine(line, allowed):
+    #find lowest occurence of any number (digit or word)
+    lowIndex = 99999999
+    first = None
+    for possibility in allowed:
+        try:
+            newIndex = line.index(possibility)
+            if newIndex < lowIndex:
+                lowIndex = newIndex
+                temp = allowed.index(possibility)
+                #convert index to correct digit
+                if temp > 8:
+                    first = str(temp-8)
+                else:
+                    first = str(temp+1)
+        except:
+            continue
+    return first
+
 f = open("day1Input.txt")
 lines = f.readlines()
 total = 0
+allowed = list("123456789") + ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+#by reversing the line and all the search parameters, you will find the last digit with exactly the same method
+revAllowed = list("123456789")
+for i in range(9,18):
+    revAllowed.append(allowed[i][::-1])
+
 for line in lines:
-    #search from start of line for 1st digit
-    for i in range(len(line)):
-        if line[i].isnumeric():
-            first = line[i]
-            break
-    #search from end of line for last digit
-    for i in range(len(line)-1, -1, -1):
-        if line[i].isnumeric():
-            last = line[i]
-            break
-    total += int(first+last)
+   total += int(searchLine(line, allowed) + searchLine(line[::-1], revAllowed))
 print(total)
