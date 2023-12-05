a3a1ed74feb30a32a61637e90a8a4552687653af
ryanbutbored <94403867+ryanbutbored@users.noreply.github.com>
2023-12-05 09:35:32 +0000

    day 1 - part 1

---
+++ Day 1.py
@@ -0,0 +1,16 @@
+f = open("day1Input.txt")
+lines = f.readlines()
+total = 0
+for line in lines:
+    #search from start of line for 1st digit
+    for i in range(len(line)):
+        if line[i].isnumeric():
+            first = line[i]
+            break
+    #search from end of line for last digit
+    for i in range(len(line)-1, -1, -1):
+        if line[i].isnumeric():
+            last = line[i]
+            break
+    total += int(first+last)
+print(total)
