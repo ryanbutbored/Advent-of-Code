53b969b86f35647ef6080830a8a47923e8c43b99
ryanbutbored <94403867+ryanbutbored@users.noreply.github.com>
2023-12-06 17:34:45 +0000

    Update Part 2.py

--- Day 5/Part 2.py
+++ Day 5/Part 2.py
@@ -1,13 +1,13 @@
 #currently all this is, is a memory leak simulator
+from time import time
 
 f = open("Day 5\\input.txt")
 lines = f.readlines()
 tempSeeds = [int(x) for x in lines[0].strip().split(": ")[1].split(" ")]
 seeds = []
 for i in range(0, len(tempSeeds), 2):
-    seeds += list(range(tempSeeds[i], tempSeeds[i] + tempSeeds[i+1]))
+    seeds.append([tempSeeds[i], tempSeeds[i]+tempSeeds[i+1] - 1])
 print("list generated")
-seeds = [int(x) for x in lines[0].strip().split(": ")[1].split(" ")]
 maps = []
 for line in lines[1:]:
     line = line.strip()
@@ -22,15 +22,23 @@ for line in lines[1:]:
     except:
         continue
 maps.append(newmap)
-i = 0
-while i < len(seeds):
-    for j in range(7):
-        seed = seeds[i]
-        for values in maps[j]:
-            minimum = values[1]
-            maximum = values[1] + values[2] - 1
-            if seed >= minimum and seed <= maximum:
-                seeds[i] = values[0] - minimum + seed
+found = False
+location = 0
+a = time()
+while not found:
+    if location % 10000 == 0:
+        print(location)
+    temp = location
+    for i in range(6, -1, -1):
+        for values in maps[i]:
+            minimum = values[0]
+            maximum = values[0] + values[2] - 1
+            if temp >= minimum and temp <= maximum:
+                temp = values[1] - minimum + temp
                 break
-    i += 1
-print(min(seeds))
+    for seed in seeds:
+        if temp >= seed[0] and temp <= seed[1]:
+            print("found", location, time()-a)
+            found = True
+            break
+    location += 1
