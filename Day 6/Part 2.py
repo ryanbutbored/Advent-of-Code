bf2e05e0f8f9330b3974274cebce8eb300c0f2bb
ryanbutbored <94403867+ryanbutbored@users.noreply.github.com>
2023-12-06 19:21:03 +0000

    Day 6

---
+++ Day 6/Part 2.py
@@ -0,0 +1,10 @@
+#BRUTE FORCE FOR THE WIN
+f = open("Day 6\\input.txt", "r")
+lines = f.readlines()
+time = int("".join([x for x in lines[0].strip().split(":")[1].split(" ") if x != ""]))
+distance = int("".join([x for x in lines[1].strip().split(":")[1].split(" ") if x != ""]))
+total = 0
+for i in range(time):
+    if i * (time-i) > distance:
+        total += 1
+print(total)
---
+++ Day 6/Part1.py
@@ -0,0 +1,17 @@
+f = open("Day 6\\input.txt", "r")
+lines = f.readlines()
+times = [int(x) for x in lines[0].strip().split(":")[1].split(" ") if x != ""]
+distances = [int(x) for x in lines[1].strip().split(":")[1].split(" ") if x != ""]
+wins = []
+for i, time in enumerate(times):
+    totalWins = 0
+    for j in range(time+1):
+        distance = j * (time-j)
+        if distance > distances[i]:
+            totalWins += 1
+    wins.append(totalWins)
+total = 1
+for win in wins:
+    total *= win
+
+print(total)
